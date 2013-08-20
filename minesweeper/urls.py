0cf909b17220ad82ee78d942ff93f761e868600c
Willy Collins <wcurtiscollins@gmail.com>
2013-08-20 16:46:14 -0400

    Modify urls

--- minesweeper/urls.py
+++ minesweeper/urls.py
@@ -1,17 +1,8 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from minesweeper.game import views as game_views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'minesweeper.views.home', name='home'),
-    # url(r'^minesweeper/', include('minesweeper.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', game_views.home),
+    url(r'^games/(?P<game>\w+)/$', game_views.showGame),
+    url(r'^games/(?P<game>\w+)/mark/$', game_views.mark)
 )
